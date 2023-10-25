1f3b4537c8acd81ebdb8a1abf2379ca780cc39f2
junderw <jonathan.underwood4649@gmail.com>
2023-10-24 23:12:23 -0700

    Use Cargo.toml data for USER_AGENT

--- src/consts.rs
+++ src/consts.rs
@@ -1,4 +1,4 @@
-pub const USER_AGENT: &str = "hibp_password_downloader v0.1.0-rust";
+pub const USER_AGENT: &str = concat!("Rust ", env!("CARGO_PKG_NAME"), " ", env!("CARGO_PKG_VERSION"));
 pub const HIBP_ROOT: &str = "https://api.pwnedpasswords.com/range/";
 pub const BEGIN: u32 = 0;
 pub const END: u32 = 0xFFFFF;
